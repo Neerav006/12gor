cc1782c1d85421136f83a81f6f9d5c461959912a
Neerav <fbnirav@gmail.com>
2018-10-25 16:14:43 +0530

    add team player api corrected

--- app/src/main/java/com/bargor/samaj/view/RamatActivity.java
+++ app/src/main/java/com/bargor/samaj/view/RamatActivity.java
@@ -4,6 +4,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.bargor.samaj.R;
@@ -48,5 +50,28 @@ public class RamatActivity extends AppCompatActivity {
         ft.add(R.id.content_activity_ramat, new SelectGameFragment());
         ft.commit();
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+         getMenuInflater().inflate(R.menu.menu_myteam,menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_menu_my_team){
+         // TODO myteam api
+
+            return true;
+        }
+
+
+        return false;
     }
 }
